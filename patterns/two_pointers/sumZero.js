ca6811fe15c980ee5b93c26bfb53c8fd0392860d
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-02-01 22:48:44 -0800

    added solution using the two pointers pattern

--- patterns/two_pointers/sumZero.js
+++ patterns/two_pointers/sumZero.js
@@ -1,15 +1,15 @@
 // write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero on undeifned if a pair does not exist.
-// examples: 
+// examples:
 // [-3, -2, -1, 0, 1, 2, 3]		// returns [-3, 3]
 // [-2, 0, 1, 3]				// returns undefined
 // [1, 2, 3]					// undefined
 
 // solution 1: Time Complexity => O(n^2)
 function sumToZero(arr) {
-  for(let i = 0; i < arr.length; i++) {
-    for(let j = i + 1; j < arr.length; j++) {
-      if(arr[i] + arr[j] === 0) {
-        return [arr[i], arr[j]];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]]
       }
     }
   }
@@ -17,19 +17,18 @@ function sumToZero(arr) {
 
 // solution 2: Time Complexity => O(n)
 function sumZero(arr) {
-	let left = 0; 
-	let right = arr.length - 1;
-  
-	while(left < right) {
-	  let sum = arr[left] + arr[right];
-	  
-	  if(sum === 0) {
-		return [arr[left], arr[right]]
-	  } else if(sum > 0) {
-		right--;
-	  } else {
-		left++;
-	  }
-	}
-} 
+  let left = 0
+  let right = arr.length - 1
 
+  while (left < right) {
+    let sum = arr[left] + arr[right]
+
+    if (sum === 0) {
+      return [arr[left], arr[right]]
+    } else if (sum > 0) {
+      right--
+    } else {
+      left++
+    }
+  }
+}
