1581c939a106dd9123e23c494a228a4afc9c770a
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-02-05 16:50:51 -0800

    solution one

--- patterns/frequency counter/anagrams/anagrams.js
+++ patterns/frequency counter/anagrams/anagrams.js
@@ -1,7 +1,7 @@
 // given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another.
 // Example: cinema => iceman 
 
-// solution 1: 
+// solution 1: naive solution 
 function isAnagram(str1, str2) {
 	const sortedStr1 = str1.split('').sort().join('');
 	const sortedStr2 = str2.split('').sort().join('');
@@ -9,3 +9,32 @@ function isAnagram(str1, str2) {
 	return sortedStr1 === sortedStr2;
 }
 
+// solution 2: frequency counter 
+function validAnagram(strOne, strTwo) {
+	const strOneCounter = {};
+	const strTwoCounter = {};
+  
+	for(let i = 0; i < strOne.length; i++) {
+	  if(!strOneCounter[strOne[i]]) {
+		strOneCounter[strOne[i]] = 1;
+	  } else {
+		strOneCounter[strOne[i]]++;
+	  }
+	}
+  
+	for(let i = 0; i < strTwo.length; i++) {
+	  if(!strTwoCounter[strTwo[i]]) {
+		strTwoCounter[strTwo[i]] = 1;
+	  } else {
+		strTwoCounter[strTwo[i]]++;
+	  }
+	}
+  
+	for (const letter in strOneCounter) {
+	  if(strOneCounter[letter] !== strTwoCounter[letter]) {
+		return false;
+	  }
+	}
+  
+	return true;
+  }
