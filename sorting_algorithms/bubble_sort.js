55d250284477b556f2637052657ae164e701e1ec
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-02-11 15:48:23 -0800

    added bubble sort implementation

--- sorting_algorithms/bubble_sort.js
+++ sorting_algorithms/bubble_sort.js
@@ -1,9 +1,22 @@
 // write an implementation of bubble sort in JavaScript.
 
 // solution 1: Naive Bubble Sort
-function bubbleSort(arr) {
+function naiveBubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return arr;
+}
+
+// solution 2: Bubble Sort
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
