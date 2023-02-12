88e3280f82d56cbb080111fbf04d197a07b449fc
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-02-12 15:49:58 -0800

    added optimized bubble sort implementation

--- sorting_algorithms/bubble_sort.js
+++ sorting_algorithms/bubble_sort.js
@@ -25,3 +25,21 @@ function bubbleSort(arr) {
 
   return arr;
 }
+
+// solution 3: optimized Bubble Sort
+function optimizedBubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    let noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) {
+      break;
+    }
+  }
+
+  return arr;
+}
