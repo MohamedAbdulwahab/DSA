287fb2e17a2663ae9cd4fa5166ea75e563ba53d8
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-03-03 02:59:32 -0800

    added addEdge method to the graph class

--- data_structures/8. graphs/graph.js
+++ data_structures/8. graphs/graph.js
@@ -12,4 +12,13 @@ class Graph {
       this.adjacencyList[name] = [];
     }
   }
+
+  /* addEdge: adds an edge (connection) between two vertices [assuming a non-directed graph] */
+  addEdge(vertexOne, vertexTwo) {
+    /* add an edge from V1 to V2 */
+    this.adjacencyList[vertexOne].push(vertexTwo);
+
+    /* add an edge from V2 to V1 */
+    this.adjacencyList[vertexTwo].push(vertexOne);
+  }
 }
